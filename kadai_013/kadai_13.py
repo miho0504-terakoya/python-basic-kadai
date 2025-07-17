a1c2434c2474f25519c51ed9d7950c0713c2c20f
miho0504-terakoya <miho.okt54@gmail.com>
2025-07-17 21:02:16 +0900

    追加

---
+++ kadai_013/kadai_13.py
@@ -0,0 +1,12 @@
+def calc_total_price(price, tax_rate):
+    tax_amount = price * (tax_rate / 100)
+    total_price = price + tax_amount
+    return total_price
+
+
+price = 110
+tax_rate = 10
+
+# 関数を呼び出して結果を表示
+total = calc_total_price(price, tax_rate)
+print(f"{total}円")
