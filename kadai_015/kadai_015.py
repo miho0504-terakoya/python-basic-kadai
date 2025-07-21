36dfc3827c3384116389abbba7ff8b40ca510bd9
miho0504-terakoya <miho.okt54@gmail.com>
2025-07-21 17:09:10 +0900

    追加

--- kadai_015/kadai_015.py
+++ kadai_015/kadai_015.py
@@ -2,15 +2,11 @@ class Human:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        
-    def printonfo(self):
-        print("名前:", self.name)
-        print("年齢:", self.age)
-   
-#　インスタンス化する
-user = Human("侍太郎", 36,)
-    
-    
-#　属性にアクセスし、値を出力する
-print(user.name)
-print(user.age)
+
+    def printinfo(self):
+        print(self.name)
+        print(self.age)
+
+
+person = Human("佐藤", 25)
+person.printinfo()
